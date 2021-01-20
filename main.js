913caa9b936925f4fcb12cf12cc744a4ee3dc4cf
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-20 18:19:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const readlineSync = require("readline-sync");
 let entry = 1;
 let sum = 0;
 let totalSum = 0;
-let entry1 = 1;
 while (entry > 0) {
   entry = Number(readlineSync.question("Apartments price \n"));
   if (entry > 0) {
@@ -12,20 +11,20 @@ while (entry > 0) {
     totalSum += entry;
   }
 }
-console.log(`${sum} apartments have registed `);
+console.log(`${sum} Apartments have registed `);
 avg = totalSum / 4;
 console.log(` The average price for rent is ${avg}`);
 
-while (entry1 > 0) {
-  entry1 = String(readlineSync.question("give numbers"));
-  
-  if (entry1 > avg) {
-    console.log("Above average price");
-  } else if (entry1 < avg) {
-    console.log("Above below price");
+while (true) {
+  entry = String(readlineSync.question("Give numbers"));
+  if (entry < 1) {
+    console.log("Quit program");
+    break;
   }
-  if (entry1<=0){
-    console.log("quit program")
-}
+  if (entry > avg) {
+    console.log("Above average price");
+  } else {
+    console.log("Bellow average price");
+  }
 }
 
